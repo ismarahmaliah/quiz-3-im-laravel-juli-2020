2bb66f7e2c6d56ca293ace38f832948d3c8ecb0c
ismarahmaliah <ismarahmaliah2407@gmail.com>
2020-08-09 22:40:10 +0700

    kak ini aku belum ngisi apa2. kalo bisa boleh mintah tambahan waktunya kak. terima kasih

---
+++ database/migrations/2020_08_09_151454_create_proyek_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProyekTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('proyek', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('nama_proyek');
+            $table->string('deskripsi');
+            $table->timestamps('tanggal_mulai');
+            $table->timestamps('tanggal_deadline');
+            $table->boolean('status');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('proyek');
+    }
+}
